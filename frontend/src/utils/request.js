6516c30877bcf7a0751cb34ce64276138ee627a3
Mohammed <mohamedknd123@gmail.com>
2026-09-05 10:37:10 +0100

    Connect frontend to deployed backend

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://blog-app-pd8m.onrender.com",
 });
 
-export default request
+export default request;
