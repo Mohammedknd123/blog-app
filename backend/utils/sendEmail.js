e9f73ea3f8eacbaaab24d14b2b7047c70158ef06
Mohammed <mohamedknd123@gmail.com>
2026-09-05 16:27:03 +0100

    fixing bugs

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,31 +6,23 @@ module.exports = async (userEmail, subject, htmlTemplate) => {
       host: "smtp.gmail.com",
       port: 587,
       secure: false,
-
       auth: {
         user: process.env.APP_EMAIL_ADDRESS,
         pass: process.env.APP_EMAIL_PASSWORD,
       },
-
       tls: {
         rejectUnauthorized: false,
       },
-
-      connectionTimeout: 20000,
-      greetingTimeout: 20000,
-      socketTimeout: 30000,
     });
 
     const info = await transporter.sendMail({
-      from: `"BlogApp" <${process.env.APP_EMAIL_ADDRESS}>`,
+      from: `"Blog App" <${process.env.APP_EMAIL_ADDRESS}>`,
       to: userEmail,
       subject: subject,
       html: htmlTemplate,
     });
 
     console.log("Email sent successfully:", info.messageId);
-
-    return info;
   } catch (error) {
     console.error("Email sending failed:", error);
 
